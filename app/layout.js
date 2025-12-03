83429449014819f2874776f510f2f3175d5142a7
malkiyahitesh <hiteshmalkiya8@gmail.com>
2025-12-03 13:32:14 +0530

    add meta tag

--- app/layout.js
+++ app/layout.js
@@ -46,6 +46,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        
+  <meta charset="UTF-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </head>
       <body>
         <Navbar />
         <City />
